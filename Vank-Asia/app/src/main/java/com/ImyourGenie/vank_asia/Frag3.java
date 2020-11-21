3081d6de6f9764082e93672c85081cb463afe086
leyujin1007 <leyujin1007@naver.com>
2020-11-21 17:21:38 +0900

    update

--- Vank-Asia/app/src/main/java/com/ImyourGenie/vank_asia/Frag3.java
+++ Vank-Asia/app/src/main/java/com/ImyourGenie/vank_asia/Frag3.java
@@ -1,11 +1,15 @@
 package com.ImyourGenie.vank_asia;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -17,8 +21,12 @@ import java.util.HashMap;
 public class Frag3 extends Fragment {
     private View view;
 
-    ImageView imageView;
+    Button button1;
+    Button button2;
+
+    ImageView imageView1;
     ImageView imageView2;
+
 
 
     @Nullable
@@ -27,12 +35,17 @@ public class Frag3 extends Fragment {
         view = inflater.inflate(R.layout.frag3,container,false);
 
 
+        button1 = (Button) view.findViewById(R.id.buttontouch_1);
+        button2 = (Button) view.findViewById(R.id.buttontouch_2);
 
-        imageView = (ImageView) view.findViewById(R.id.image_vank);
-        imageView2 = (ImageView) view.findViewById(R.id.image_we);
 
+        imageView1 = (ImageView) view.findViewById(R.id.imagetouch_1); //손가락
+        imageView2 = (ImageView) view.findViewById(R.id.imagetouch_2); //손가락
+        Animation startAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.blink_animation);
+        imageView1.startAnimation(startAnimation);
+        imageView2.startAnimation(startAnimation);
 
-        imageView.setOnClickListener(new AdapterView.OnClickListener() {
+        button1.setOnClickListener(new AdapterView.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Frag3.super.getContext(), vank_detail.class);
@@ -40,7 +53,7 @@ public class Frag3 extends Fragment {
             }
         });
 
-        imageView2.setOnClickListener(new AdapterView.OnClickListener() {
+        button2.setOnClickListener(new AdapterView.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Frag3.super.getContext(), we_detail.class);
@@ -48,6 +61,6 @@ public class Frag3 extends Fragment {
             }
         });
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return view;
     }
 }
--- Vank-Asia/app/src/main/java/com/ImyourGenie/vank_asia/vank_detail.java
+++ Vank-Asia/app/src/main/java/com/ImyourGenie/vank_asia/vank_detail.java
@@ -1,10 +1,17 @@
 package com.ImyourGenie.vank_asia;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class vank_detail extends Activity {
     //https://simsi6.tistory.com/53
     // 유튜브 걸기
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_vank);
+    }
 }
