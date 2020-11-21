3b9fe22a27c0a44e9eecffaa3c4b7e1756058e29
leyujin1007 <leyujin1007@naver.com>
2020-11-22 00:31:33 +0900

    update

--- Vank-Asia/app/src/main/java/com/ImyourGenie/vank_asia/we_detail.java
+++ Vank-Asia/app/src/main/java/com/ImyourGenie/vank_asia/we_detail.java
@@ -1,6 +1,17 @@
 package com.ImyourGenie.vank_asia;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class we_detail extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_we);
+
+
+    }
 }
